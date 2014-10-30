69a4aa3e5e1056a167e4c7e0975b028f16310bd1
Jesús Carro <chuscarro@gmail.com>
2014-10-30 08:42:21 +0100

    Adding the original files

---
+++ src/cellml2c.cpp
@@ -0,0 +1,38 @@
+#include <IfaceCellML_APISPEC.hxx>
+#include <IfaceAnnoTools.hxx>
+#include <IfaceCeLEDSExporter.hxx>
+#include <CeLEDSExporterBootstrap.hpp>
+#include <CellMLBootstrap.hpp>
+#include <fstream>
+#include <string>
+#include <cstring>
+
+using namespace iface::cellml_api;
+using namespace iface::cellml_services;
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+        const size_t cSize = strlen(argv[1])+1;
+        wchar_t* wc = new wchar_t[cSize];
+        mbstowcs (wc, argv[1], cSize);
+        wstring wide(wc);	
+  
+	
+	CellMLBootstrap* bootstrap = CreateCellMLBootstrap();
+	DOMModelLoader* ml = bootstrap->modelLoader();
+	Model* model = ml->loadFromURL(wide);
+
+	CeLEDSExporterBootstrap* celedsexporterb = CreateCeLEDSExporterBootstrap();
+
+	CodeExporter* ce = celedsexporterb->createExporter(L"C.xml");
+
+	wstring code = ce->generateCode(model);
+
+	wofstream wofs(argv[2]);
+        wofs << code;
+
+	cout << "File " << argv[2] << " created." <<endl;
+	return 0;
+}
