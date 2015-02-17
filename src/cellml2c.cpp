6a6f12eb0beb554459ad15c89bbff5119f77777b
Jesús Carro <chuscarro@gmail.com>
2015-02-18 00:18:54 +0100

    License

--- src/cellml2c.cpp
+++ src/cellml2c.cpp
@@ -18,7 +18,7 @@
 
   ---------------------------------------------------------------------------
 
-   Copyright 2015 J. Carro
+   Copyright 2015 J. Carro; J. Castro
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
