560b4346590373ba099797a0b80842a6089bc617
Jesús Carro <chuscarro@gmail.com>
2015-02-18 00:17:23 +0100

    License

--- src/cellml2c.cpp
+++ src/cellml2c.cpp
@@ -1,3 +1,39 @@
+/*===========================================================================
+  
+                            --- DENIS Project ---
+                              -----------------
+
+     Distributed computing
+     Electrophysiologycal Models
+     Networking colaboration
+     In Silico research
+     Sharing Knowledge
+                                                
+  ---------------------------------------------------------------------------
+
+                                                        San Jorge University
+                                                       School of Engineering
+ 
+                                                           http://eps.usj.es
+
+  ---------------------------------------------------------------------------
+
+   Copyright 2015 J. Carro
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+  ===========================================================================*/
+
 #include <IfaceCellML_APISPEC.hxx>
 #include <IfaceAnnoTools.hxx>
 #include <IfaceCeLEDSExporter.hxx>
